27fd61d297eeef942c01b73a4bdeec628261f018
Lê Bảo Long <longle12042006a@gmail.com>
2023-09-17 13:14:02 +0700

    upload layout

--- components/misc/layout.tsx
+++ components/misc/layout.tsx
@@ -10,7 +10,7 @@ type Props = {
 const Layout = ({ children }: Props) => {
   return (
     <div className="flex flex-col min-h-screen overflow-hidden">
-      <Meta />
+      {/* <Meta /> */}
       <Header />
       <main className="grow">
         {children}
